eadeefed8add363a0e9bbad666ce6910bd408f03
Caio Mitsuo <caiomitsuo@MacBook-Pro-de-Caio.local>
2020-07-15 11:31:06 -0300

    Api FInalizada

---
+++ apifuncionarios/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>LISTA</h1>
+    <EmployeeList />
+  </div>
+</template>
+<script>
+import EmployeeList from "./components/EmployeeList";
+export default {
+  name: "App",
+  components: {
+    EmployeeList,
+  },
+};
+</script>
+<style></style>
